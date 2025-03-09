e6f8d6f88e1f8fed39735677a051871984aa6732
Prajwal-PL <prajwalpl096@gmail.com>
2025-03-09 21:10:56 +0530

    updating route paths

--- server/src/index.js
+++ server/src/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/auth", authRoute);
+app.use("/api/auth", authRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
