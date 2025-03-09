322ec2a60ceefe58c3f4743cefd675dc6bb0bb3a
Prajwal-PL <prajwalpl096@gmail.com>
2025-03-09 20:55:42 +0530

    fixing syntax errors

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { authFunction } from "../controllers/auth.controller";
+import { authFunction } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/auth/:role", authFunction);
+
+export default router;
