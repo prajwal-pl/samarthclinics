11bce4208944a48806f3017b0541c1cc718e3825
Prajwal-PL <prajwalpl096@gmail.com>
2025-03-22 15:09:46 +0530

    Adding booking schema

---
+++ server/models/booking.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const BookingSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    doctor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Booking = mongoose.model("Booking", BookingSchema);
